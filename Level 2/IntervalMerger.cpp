51e20ad7118df688ac0301e6e4b6a9e9fb23a09e
AdityaVKochar <adityavardhankochar@gmail.com>
2025-03-07 23:07:43 +0530

    LEVEL 3

--- Level 2/IntervalMerger.cpp
+++ Level 2/IntervalMerger.cpp
@@ -7,19 +7,17 @@ using namespace std;
 
 class IntervalMerger {
 private:
-    set<pair<int, int>> intervals; // Using a set to maintain sorted non-overlapping intervals
+    set<pair<int, int>> intervals; 
 
 public:
     void addInterval(int start, int end) {
         pair<int, int> newInterval(start, end);
         
-        // Find intervals to merge
-        auto it = intervals.lower_bound({start, end}); // Find the first interval >= newInterval
+        auto it = intervals.lower_bound({start, end});
         
-        // Merge with intervals to the right
         while (it != intervals.end() && it->first <= end) {
             end = max(end, it->second);
-            it = intervals.erase(it); // Erase and get the next iterator
+            it = intervals.erase(it);
         }
 
         // Merge with intervals to the left
@@ -38,21 +36,3 @@ public:
     }
 };
 
-int main() {
-    IntervalMerger merger;
-    merger.addInterval(1, 5);
-    merger.addInterval(6, 8);
-    merger.addInterval(4, 7);
-
-    vector<pair<int, int>> result = merger.getIntervals();
-    cout << "[";
-    for (int i = 0; i < result.size(); ++i) {
-        cout << "[" << result[i].first << ", " << result[i].second << "]";
-        if (i < result.size() - 1) {
-            cout << ", ";
-        }
-    }
-    cout << "]" << endl; // Output: [[1, 8]]
-
-    return 0;
-}
